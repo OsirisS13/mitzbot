9e666ab447d695b2e63852f7495f0aeed1490bd6
OsirisS13 <shaffwa@gmail.com>
2017-04-15 17:30:25 +0100

    Create config file
    Config file for pages to check, column to write to etc

---
+++ config.py
@@ -0,0 +1,15 @@
+#change these details with the pages you want to check, along with the columns to store the info in.  
+#FB App ID and Secret needed to check facebook pages
+
+Mitzbot_FB_APP_ID = ''
+Mitzbot_FB_APP_SECRET = ''
+FBPageName = ""
+twitterusername = ''
+IGusername = ""
+spreadsheet = ""
+worksheet = ""
+fbcolumn = 
+igcolumn = 
+twittercolumn = 
+#storage file that the mitzbot_update_googlesheets module stores and reads the last written row from
+storagefile = "last_written_row.txt"
